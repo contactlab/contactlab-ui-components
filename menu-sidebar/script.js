e68751c889cf2438f899abb94cf2d932c9d4eaf8
Salvatore Laisa <moebiusmania@gmail.com>
2015-10-09 16:17:46 +0200

    fixed menu component

--- menu-sidebar/script.js
+++ menu-sidebar/script.js
@@ -27,6 +27,8 @@ Polymer({
 		}
 	},
 	attached: function(){
+		this._iosMenu();
+
 		setTimeout(function(){
 			$('menu-clab .first-level-menu>li>a').each(function(i, e){					
 				if(location.hash.search(e.getAttribute('href')) > -1){
@@ -37,9 +39,44 @@ Polymer({
 			});
 		},10)
 	},
-	_activeItem: function(evt){
-		$('menu-clab li').removeClass('active');
-		evt.currentTarget.parentNode.classList.add('active');
+	_openItem: function(evt){
+		$('menu-clab .first-level-menu>li>a').each(function(i, e){					
+			if(location.hash.search(e.getAttribute('href')) > -1){
+				e.parentNode.classList.add('active');
+			}else{
+				e.parentNode.classList.remove('active');
+			}
+		});
+		$('menu-clab .first-level-menu>li').each(function(i, e){					
+			e.classList.remove('open');
+		});
+		if(evt.currentTarget.nextElementSibling.tagName === 'UL'){
+			evt.preventDefault();
+			evt.currentTarget.parentNode.classList.add('open');
+			$('menu-clab .first-level-menu>li>a').each(function(i,e){
+				e.parentNode.classList.remove('active');
+			});
+			evt.currentTarget.parentNode.classList.add('active');
+		}
+	},
+	_iosMenu: function(){
+		$('body main').on('click', function(evt){
+			console.log(evt.target.nodeName);
+			switch(evt.target.nodeName){
+				case 'INPUT':
+				case 'BUTTON':
+				case 'TEXTAREA':
+				case 'LABEL':
+					return true;
+					break;
+				default:
+					document.querySelector('#logo a').focus();
+					$('menu-clab .first-level-menu>li').each(function(i,e){
+						e.classList.remove('open');
+					});
+					break;
+			}
+		});
 	},
 	_reduce: function(){
 		document.body.classList.toggle('main-sidebar-small');
