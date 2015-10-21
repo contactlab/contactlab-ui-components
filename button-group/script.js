a0f4e23f50571d31ad53718796bf31a693699b5f
Salvatore Laisa <moebiusmania@gmail.com>
2015-10-21 12:47:22 +0200

    fixed <group-clab> issue

--- button-group/script.js
+++ button-group/script.js
@@ -12,21 +12,21 @@ Polymer({
 		value: {
 			type: Number,
 			value: 0,
-			observer: '_initilize',
+			observer: '_initialize',
 			reflectToAttribute: true
 		}
 	},
 	attached: function(){
-		this._initilize();
+		this._initialize();
 	},
-	_initilize: function(){
+	_initialize: function(){
 		var btns = this.getContentChildren();
 		for(var i = 0; i < btns.length; i++){
-			btns[i].classList.remove('full');
+			(typeof btns[i].appearance === 'string') ? btns[i].appearance = '' : null;
 			btns[i].setAttribute('data-i',i);
 			btns[i].addEventListener('click',this._selectElement.bind(this))
 		}
-		btns[this.value].classList.add('full');
+		(typeof btns[this.value].appearance === 'string') ? btns[this.value].appearance = 'full' : null;
 	},
 	_computeGroupClass: function(type,small){
 		var arr = ['buttons-group',type];
@@ -37,9 +37,9 @@ Polymer({
 		evt.preventDefault();
 		var btns = this.getContentChildren();
 		for(var i = 0; i < btns.length; i++){
-			btns[i].classList.remove('full');
+			btns[i].appearance = '' ;
 		}
-		evt.target.classList.add('full');
-		this.value = parseInt(evt.target.getAttribute('data-i'));
+		this.value = parseInt(evt.target.parentNode.getAttribute('data-i'));
+		btns[this.value].appearance = 'full';
 	}
 });
