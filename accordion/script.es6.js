80d805ea8f434963919ecd4ac5bdc3413301ae5c
Laura Santalucia <laura.santalucia@contactlab.com>
2015-12-01 17:37:07 +0100

    accordion component added

---
+++ accordion/script.es6.js
@@ -0,0 +1,28 @@
+class AccordionClab {
+
+	beforeRegister(){
+		this.is = "accordion-clab";
+		this.properties = {
+			title: {
+				type: String,
+				value: 'Title'
+			},
+			type: {
+				type: String,
+				value: ''
+			}
+		}
+	}
+
+	_computeType(type){
+		return ['accordion-block',type].join(' ');
+	}
+
+	_toggleActive(){
+		this.$$('.accordion-block').classList.toggle('active');
+	}
+
+}
+
+
+Polymer(AccordionClab);
