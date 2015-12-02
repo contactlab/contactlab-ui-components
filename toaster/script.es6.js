521484d01a9ebfb131dedcdb59b54e1ed1e516c2
Laura Santalucia <laura.santalucia@contactlab.com>
2015-12-02 11:01:32 +0100

    added toaster component

---
+++ toaster/script.es6.js
@@ -0,0 +1,75 @@
+class ToasterClab {
+
+	beforeRegister(){
+		this.is = "toaster-clab";
+		this.properties = {
+			right: {
+				type: Boolean,
+				value: false
+			},
+			message: {
+				type: String
+			},
+			callerType: {
+				type: String,
+				value: 'primary'
+			},
+			actionType: {
+				type: String,
+				value: 'success'
+			},
+			actionText:{
+				type: String,
+				value: 'OK'
+			},
+
+			id: {
+				type: Object,
+				computed: 'computeIds(right)'
+			},
+			compCallerType: {
+				type: String,
+				computed: "computeType('btn', callerType)"
+			},
+			compActionType: {
+				type: String,
+				computed: "computeType('btn small flat', actionType)"
+			}
+		}
+	}
+
+	computeIds(isRight){
+		this.name = 'right';
+		if(!isRight) this.name = 'left';
+
+		return {
+			btn: "toast-"+ this.name +"-btn",
+			toast: "toast-"+ this.name
+		}
+	}
+
+	computeType(def,type){
+		console.log(def,type);
+		return [def,type].join(' ');
+	}
+
+
+
+	_toggleToast(evt){
+		if(evt.target.id){
+			this.querySelector('#toast-'+this.name).classList.add('visible');
+		} else {
+			this.querySelector('#toast-'+this.name).classList.remove('visible');
+		}
+	}
+
+	_computeWrapperType(isRight){
+		let type = 'toast-wrapper';
+		if(isRight) type += ' right';
+		return type;
+	}
+
+}
+
+
+Polymer(ToasterClab);
