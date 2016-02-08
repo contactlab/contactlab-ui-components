995747db6e83fe5e12914530397ed1542fbc6265
Laura Santalucia <laura.santalucia@contactlab.com>
2016-02-08 16:46:52 +0100

    accordion animation handled with web-animations.js

--- accordion/script.es6.js
+++ accordion/script.es6.js
@@ -10,16 +10,124 @@ class AccordionClab {
 			type: {
 				type: String,
 				value: ''
+			},
+			open: {
+				type:Boolean,
+				value:false,
+				observer: '_animateOpenClose'
 			}
 		}
 	}
+
+	attached(){
+		this.height = this.querySelector('.accordion-content').clientHeight;
+
+		// Preparing the animations
+		this.block=this.querySelector('.accordion-block');
+		this.content=this.querySelector('.accordion-content');
+		this.content.style.height=0;
+		this.content.style.opacity=1;
+
+		let height=[
+			{height: '0'},
+			{height: this.height+'px'}
+		];
+		let bgColor=[
+			{'background-color': 'rgba(255, 255, 255, 0)'},
+			{'background-color': 'rgba(255, 255, 255, 1)'}
+		];
+		let margin=[
+			{'margin-bottom': '0'},
+			{'margin-bottom': '20px'}
+		];
+		let padding=[
+			{'padding': '0'},
+			{'padding': '20px'}
+		];
+		let shadow=[
+			{'box-shadow': '0 0 0 0 rgba(0, 0, 0, 0)'},
+			{'box-shadow': '0 2px 5px 0 rgba(0, 0, 0, 0.16)'}
+		];
+
+		this.openAccordion = new GroupEffect([
+			new KeyframeEffect(this.content, height, {
+				duration:190,
+				fill:'forwards',
+				direction: 'normal'
+			}),
+			new KeyframeEffect(this.block, bgColor, {
+				duration:100,
+				fill:'forwards',
+				direction: 'normal'
+			}),
+			new KeyframeEffect(this.block, margin, {
+				duration:100,
+				fill:'forwards',
+				direction: 'normal'
+			}),
+			new KeyframeEffect(this.block, padding, {
+				duration:100,
+				fill:'forwards',
+				direction: 'normal'
+			}),
+			new KeyframeEffect(this.block, shadow, {
+				duration:100,
+				fill:'forwards',
+				direction: 'normal'
+			})
+		]);
+
+		this.closeAccordion = new GroupEffect([
+			new KeyframeEffect(this.content, height, {
+				duration:100,
+				fill:'forwards',
+				direction: 'reverse'
+			}),
+			new KeyframeEffect(this.block, bgColor, {
+				duration:100,
+				fill:'forwards',
+				direction: 'reverse'
+			}),
+			new KeyframeEffect(this.block, margin, {
+				duration:100,
+				fill:'forwards',
+				direction: 'reverse'
+			}),
+			new KeyframeEffect(this.block, padding, {
+				duration:100,
+				fill:'forwards',
+				direction: 'reverse'
+			}),
+			new KeyframeEffect(this.block, shadow, {
+				duration:100,
+				fill:'forwards',
+				direction: 'reverse'
+			})
+		]);
+	}
+
+
+
+	_animateOpenClose(val, old){
+		if(old!=undefined){
+			if(val){
+				let player = document.timeline.play(this.openAccordion);
+				this.querySelector('.accordion-title').classList.add('active');
+			} else {
+				let player = document.timeline.play(this.closeAccordion);
+				this.querySelector('.accordion-title').classList.remove('active');
+			}
+		}
+	}
+
+
 
 	_computeType(type){
 		return ['accordion-block',type].join(' ');
 	}
 
 	_toggleActive(){
-		this.querySelector('.accordion-block').classList.toggle('active');
+		this.open=!this.open;
 	}
 
 }
--- accordion/script.js
+++ accordion/script.js
@@ -21,8 +21,87 @@ var AccordionClab = (function () {
 				type: {
 					type: String,
 					value: ''
+				},
+				open: {
+					type: Boolean,
+					value: false,
+					observer: '_animateOpenClose'
 				}
 			};
+		}
+	}, {
+		key: 'attached',
+		value: function attached() {
+			this.height = this.querySelector('.accordion-content').clientHeight;
+
+			// Preparing the animations
+			this.block = this.querySelector('.accordion-block');
+			this.content = this.querySelector('.accordion-content');
+			this.content.style.height = 0;
+			this.content.style.opacity = 1;
+
+			var height = [{ height: '0' }, { height: this.height + 'px' }];
+			var bgColor = [{ 'background-color': 'rgba(255, 255, 255, 0)' }, { 'background-color': 'rgba(255, 255, 255, 1)' }];
+			var margin = [{ 'margin-bottom': '0' }, { 'margin-bottom': '20px' }];
+			var padding = [{ 'padding': '0' }, { 'padding': '20px' }];
+			var shadow = [{ 'box-shadow': '0 0 0 0 rgba(0, 0, 0, 0)' }, { 'box-shadow': '0 2px 5px 0 rgba(0, 0, 0, 0.16)' }];
+
+			this.openAccordion = new GroupEffect([new KeyframeEffect(this.content, height, {
+				duration: 190,
+				fill: 'forwards',
+				direction: 'normal'
+			}), new KeyframeEffect(this.block, bgColor, {
+				duration: 100,
+				fill: 'forwards',
+				direction: 'normal'
+			}), new KeyframeEffect(this.block, margin, {
+				duration: 100,
+				fill: 'forwards',
+				direction: 'normal'
+			}), new KeyframeEffect(this.block, padding, {
+				duration: 100,
+				fill: 'forwards',
+				direction: 'normal'
+			}), new KeyframeEffect(this.block, shadow, {
+				duration: 100,
+				fill: 'forwards',
+				direction: 'normal'
+			})]);
+
+			this.closeAccordion = new GroupEffect([new KeyframeEffect(this.content, height, {
+				duration: 100,
+				fill: 'forwards',
+				direction: 'reverse'
+			}), new KeyframeEffect(this.block, bgColor, {
+				duration: 100,
+				fill: 'forwards',
+				direction: 'reverse'
+			}), new KeyframeEffect(this.block, margin, {
+				duration: 100,
+				fill: 'forwards',
+				direction: 'reverse'
+			}), new KeyframeEffect(this.block, padding, {
+				duration: 100,
+				fill: 'forwards',
+				direction: 'reverse'
+			}), new KeyframeEffect(this.block, shadow, {
+				duration: 100,
+				fill: 'forwards',
+				direction: 'reverse'
+			})]);
+		}
+	}, {
+		key: '_animateOpenClose',
+		value: function _animateOpenClose(val, old) {
+			if (old != undefined) {
+				if (val) {
+					var player = document.timeline.play(this.openAccordion);
+					this.querySelector('.accordion-title').classList.add('active');
+				} else {
+					var player = document.timeline.play(this.closeAccordion);
+					this.querySelector('.accordion-title').classList.remove('active');
+				}
+			}
 		}
 	}, {
 		key: '_computeType',
@@ -32,7 +111,7 @@ var AccordionClab = (function () {
 	}, {
 		key: '_toggleActive',
 		value: function _toggleActive() {
-			this.querySelector('.accordion-block').classList.toggle('active');
+			this.open = !this.open;
 		}
 	}]);
 
