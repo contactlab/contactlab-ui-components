09a93754fa46385b7d19e39c4a0847d96fbea78b
Laura <laura.santalucia@contactlab.com>
2016-02-26 17:54:06 +0100

    tooltip-clab updated
    Removed dependency & refactored

--- tooltip/script.es6.js
+++ tooltip/script.es6.js
@@ -11,39 +11,167 @@ class TooltipClab{
 				type: String,
 				value: ""
 			},
-			ttType: {
-				type: String,
-				value: ""
+			visible: {
+				type: Boolean,
+				value: false,
+				observer: '_observVisibility'
+			},
+			wait: {
+				type:Number,
+				value:500
+			},
+
+			_dontHide: {
+				type:Boolean,
+				value:false
 			}
+		};
+		this.listeners = {
+			'mouseenter': '_handleMouseOnLabel',
+			'mouseout': '_handleMouseOnLabel'
 		}
 	}
 
 
 
-	/*---------- 
+	/*----------
 	EVENT HANDLERS
 	----------*/
-	_handleHover(evt){
-		document.querySelector('.kawo-tooltip').className = 'kawo-tooltip'; 
-		document.querySelector('.kawo-tooltip-arrow').className = 'kawo-tooltip-arrow';
-		
-		if(this.ttType){
-			document.querySelector('.kawo-tooltip').classList.add(this.ttType); 
-			document.querySelector('.kawo-tooltip-arrow').classList.add(this.ttType);
-		} 
+	_handleMouseOnLabel(evt){
+		if(evt.type=='mouseenter'){
+			console.log('mouseenter----------------->', evt);
+			// let x=evt.clientX;
+			// let y=evt.clientY;
+			// let targetRect=Polymer.dom(evt.target).node.getBoundingClientRect();
+			// document.body.addEventListener('mousemove', (e)=>{
+			// 	this.curX=e.clientX;
+			// 	this.curY=e.clientY;
+			// 	console.log('x: ', x, this.curX, targetRect.left, targetRect.right);
+			// 	console.log('starting y: '+ y, this.curY, targetRect.top, targetRect.bottom);
+			// });
+
+			this._resetTimeout();
+			if(!this.visible){
+				//console.log(evt.target, 'mouseenter');
+				this._startTimeout(()=>{
+					this.targetPosition = window.getComputedStyle(this).getPropertyValue('position');
+					if(!this.visible) this.show();
+					Polymer.dom.flush();
+
+					this._setTooltipPosition();
+					this.querySelector('.tooltip').style.opacity='1';
+				}, this.wait);
+			}
+
+		} else if(evt.type=='mouseout' && this.visible) {
+			console.log(this, 'mouseout');
+			this._startTimeout(()=>{
+				this.querySelector('.tooltip').style.opacity='0';
+				this.hide();
+			}, 100);
+
+		}
+	}
+
+	_hideTooltip(evt){
+		if(this.visible){
+			this.querySelector('.tooltip').style.opacity='0';
+			this.hide();
+		}
+
+		if(evt.type=='wheel'){
+			document.body.removeEventListener('wheel', this._hideTooltip.bind(this));
+		}
 	}
 
 
 
-	/*---------- 
+	/*----------
+	METHODS
+	----------*/
+	_startTimeout(fn, time){
+		if(this.interval) this._resetTimeout();
+		this.interval=window.setTimeout(()=>{ fn(); }, time);
+	}
+	_resetTimeout(){
+		window.clearTimeout(this.interval);
+		this.interval=undefined;
+	}
+
+	_positionHorizontal(left, right, arrowLeft){
+		let tooltip=this.querySelector('.tooltip');
+		let arrow=this.querySelector('.tooltip .arrow');
+
+		tooltip.style.left = left;
+		tooltip.style.right = right;
+		arrow.style.left = arrowLeft + 'px';
+	}
+
+	_positionVertical(tooltipTop, rotation, top, bottom){
+		let tooltip=this.querySelector('.tooltip');
+		let arrow=this.querySelector('.tooltip .arrow');
+
+		tooltip.style.top = tooltipTop + 'px';
+		arrow.style.webkitTransform = 'rotate(' + rotation + 'deg)';
+		arrow.style.msTransform = 'rotate(' + rotation + 'deg)';
+		arrow.style.transform = 'rotate(' + rotation + 'deg)';
+		arrow.style.top = top;
+		arrow.style.bottom = bottom;
+	}
+
+	_setTooltipPosition(){
+		let targetSize;
+		if(Polymer.dom(this.querySelector('.tt-label')).node.$) //if the content is a component or not
+			targetSize=Polymer.dom(this.querySelector('.tt-label')).node.children[0].getBoundingClientRect();
+		else targetSize=this.querySelector('.tt-label').getBoundingClientRect();
+		let tooltipSize = this.querySelector('.tooltip').getBoundingClientRect();
+		let center = targetSize.left + ( targetSize.width / 2 );
+
+		if ( ( tooltipSize.width / 2 ) > ( center - 20 ) ) // rientra a sinistra
+			this._positionHorizontal( '20px', 'auto', ( center - 25 ) );
+		else if ( ( center + tooltipSize.width / 2 ) > window.innerWidth - 20 ) // rientra a destra
+			this._positionHorizontal( 'auto', '20px', ( ( tooltipSize.width - ( targetSize.width / 2 ) ) - 5 ) );
+		else this._positionHorizontal( ( center - ( tooltipSize.width / 2 ) ) + 'px', 'auto', ( ( tooltipSize.width / 2 ) - 5 ) );
+		if ( ( targetSize.bottom + tooltipSize.height ) > window.innerHeight ) // allinea verticalmente
+			this._positionVertical( ( targetSize.top -  tooltipSize.height - 5 ), '-135', 'auto', '-5px' );
+		else this._positionVertical( ( targetSize.bottom + 5 ), '45', '-5px', 'auto' );
+	}
+
+
+	/*----------
+	OBSERVERS
+	----------*/
+	_observVisibility(newv, oldv){
+		if(newv){
+			document.body.addEventListener('wheel', this._hideTooltip.bind(this));
+		}
+	}
+
+
+
+	/*----------
 	COMPUTED
 	----------*/
 	_computeBtnClass(type){
-		return ['tooltip',type].join(' ');
+		var arr = ['tooltip'];
+		if(type) arr.push(type);
+		return arr.join(' ');
+	}
+
+
+
+	/*----------
+	PUBLIC
+	----------*/
+	show(){
+		this.visible=true;
+	}
+
+	hide(){
+		this.visible=false;
 	}
 
 
 }
 
 Polymer(TooltipClab);
-
--- tooltip/script.js
+++ tooltip/script.js
@@ -1,10 +1,10 @@
 "use strict";
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var TooltipClab = (function () {
+var TooltipClab = function () {
 	function TooltipClab() {
 		_classCallCheck(this, TooltipClab);
 	}
@@ -22,41 +22,179 @@ var TooltipClab = (function () {
 					type: String,
 					value: ""
 				},
-				ttType: {
-					type: String,
-					value: ""
+				visible: {
+					type: Boolean,
+					value: false,
+					observer: '_observVisibility'
+				},
+				wait: {
+					type: Number,
+					value: 500
+				},
+
+				_dontHide: {
+					type: Boolean,
+					value: false
 				}
+			};
+			this.listeners = {
+				'mouseenter': '_handleMouseOnLabel',
+				'mouseout': '_handleMouseOnLabel'
 			};
 		}
 
-		/*---------- 
+		/*----------
   EVENT HANDLERS
   ----------*/
 
 	}, {
-		key: "_handleHover",
-		value: function _handleHover(evt) {
-			document.querySelector('.kawo-tooltip').className = 'kawo-tooltip';
-			document.querySelector('.kawo-tooltip-arrow').className = 'kawo-tooltip-arrow';
+		key: "_handleMouseOnLabel",
+		value: function _handleMouseOnLabel(evt) {
+			var _this = this;
 
-			if (this.ttType) {
-				document.querySelector('.kawo-tooltip').classList.add(this.ttType);
-				document.querySelector('.kawo-tooltip-arrow').classList.add(this.ttType);
+			if (evt.type == 'mouseenter') {
+				console.log('mouseenter----------------->', evt);
+				// let x=evt.clientX;
+				// let y=evt.clientY;
+				// let targetRect=Polymer.dom(evt.target).node.getBoundingClientRect();
+				// document.body.addEventListener('mousemove', (e)=>{
+				// 	this.curX=e.clientX;
+				// 	this.curY=e.clientY;
+				// 	console.log('x: ', x, this.curX, targetRect.left, targetRect.right);
+				// 	console.log('starting y: '+ y, this.curY, targetRect.top, targetRect.bottom);
+				// });
+
+				this._resetTimeout();
+				if (!this.visible) {
+					//console.log(evt.target, 'mouseenter');
+					this._startTimeout(function () {
+						_this.targetPosition = window.getComputedStyle(_this).getPropertyValue('position');
+						if (!_this.visible) _this.show();
+						Polymer.dom.flush();
+
+						_this._setTooltipPosition();
+						_this.querySelector('.tooltip').style.opacity = '1';
+					}, this.wait);
+				}
+			} else if (evt.type == 'mouseout' && this.visible) {
+				console.log(this, 'mouseout');
+				this._startTimeout(function () {
+					_this.querySelector('.tooltip').style.opacity = '0';
+					_this.hide();
+				}, 100);
+			}
+		}
+	}, {
+		key: "_hideTooltip",
+		value: function _hideTooltip(evt) {
+			if (this.visible) {
+				this.querySelector('.tooltip').style.opacity = '0';
+				this.hide();
+			}
+
+			if (evt.type == 'wheel') {
+				document.body.removeEventListener('wheel', this._hideTooltip.bind(this));
 			}
 		}
 
-		/*---------- 
+		/*----------
+  METHODS
+  ----------*/
+
+	}, {
+		key: "_startTimeout",
+		value: function _startTimeout(fn, time) {
+			if (this.interval) this._resetTimeout();
+			this.interval = window.setTimeout(function () {
+				fn();
+			}, time);
+		}
+	}, {
+		key: "_resetTimeout",
+		value: function _resetTimeout() {
+			window.clearTimeout(this.interval);
+			this.interval = undefined;
+		}
+	}, {
+		key: "_positionHorizontal",
+		value: function _positionHorizontal(left, right, arrowLeft) {
+			var tooltip = this.querySelector('.tooltip');
+			var arrow = this.querySelector('.tooltip .arrow');
+
+			tooltip.style.left = left;
+			tooltip.style.right = right;
+			arrow.style.left = arrowLeft + 'px';
+		}
+	}, {
+		key: "_positionVertical",
+		value: function _positionVertical(tooltipTop, rotation, top, bottom) {
+			var tooltip = this.querySelector('.tooltip');
+			var arrow = this.querySelector('.tooltip .arrow');
+
+			tooltip.style.top = tooltipTop + 'px';
+			arrow.style.webkitTransform = 'rotate(' + rotation + 'deg)';
+			arrow.style.msTransform = 'rotate(' + rotation + 'deg)';
+			arrow.style.transform = 'rotate(' + rotation + 'deg)';
+			arrow.style.top = top;
+			arrow.style.bottom = bottom;
+		}
+	}, {
+		key: "_setTooltipPosition",
+		value: function _setTooltipPosition() {
+			var targetSize = undefined;
+			if (Polymer.dom(this.querySelector('.tt-label')).node.$) //if the content is a component or not
+				targetSize = Polymer.dom(this.querySelector('.tt-label')).node.children[0].getBoundingClientRect();else targetSize = this.querySelector('.tt-label').getBoundingClientRect();
+			var tooltipSize = this.querySelector('.tooltip').getBoundingClientRect();
+			var center = targetSize.left + targetSize.width / 2;
+
+			if (tooltipSize.width / 2 > center - 20) // rientra a sinistra
+				this._positionHorizontal('20px', 'auto', center - 25);else if (center + tooltipSize.width / 2 > window.innerWidth - 20) // rientra a destra
+				this._positionHorizontal('auto', '20px', tooltipSize.width - targetSize.width / 2 - 5);else this._positionHorizontal(center - tooltipSize.width / 2 + 'px', 'auto', tooltipSize.width / 2 - 5);
+			if (targetSize.bottom + tooltipSize.height > window.innerHeight) // allinea verticalmente
+				this._positionVertical(targetSize.top - tooltipSize.height - 5, '-135', 'auto', '-5px');else this._positionVertical(targetSize.bottom + 5, '45', '-5px', 'auto');
+		}
+
+		/*----------
+  OBSERVERS
+  ----------*/
+
+	}, {
+		key: "_observVisibility",
+		value: function _observVisibility(newv, oldv) {
+			if (newv) {
+				document.body.addEventListener('wheel', this._hideTooltip.bind(this));
+			}
+		}
+
+		/*----------
   COMPUTED
   ----------*/
 
 	}, {
 		key: "_computeBtnClass",
 		value: function _computeBtnClass(type) {
-			return ['tooltip', type].join(' ');
+			var arr = ['tooltip'];
+			if (type) arr.push(type);
+			return arr.join(' ');
+		}
+
+		/*----------
+  PUBLIC
+  ----------*/
+
+	}, {
+		key: "show",
+		value: function show() {
+			this.visible = true;
+		}
+	}, {
+		key: "hide",
+		value: function hide() {
+			this.visible = false;
 		}
 	}]);
 
 	return TooltipClab;
-})();
+}();
 
 Polymer(TooltipClab);
