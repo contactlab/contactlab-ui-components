d3f485b1d7c1ac0d4bfc3af9906170df21ee7265
Alessio Occhipinti <LasaleFamine@users.noreply.github.com>
2016-11-03 09:24:44 +0100

    ref #55, fix tests and demo.html (#76)

--- checkbox/script.es6.js
+++ checkbox/script.es6.js
@@ -16,60 +16,111 @@ export class CheckboxClab {
   beforeRegister() {
     this.is = "checkbox-clab";
     this.properties = {
-      labels: Array,
-      wrapperType: String,
-      active: {
-        value: [],
-        type: Array,
-        notify: true
+      /** Label of the checkbox */
+      label: {
+        type: String
       },
-      disabled: Array
+      /** Wrapper style type */
+      wrapperType: {
+        type: String
+      },
+      /** Determine if the check is checked or not */
+      active: {
+        type: Boolean,
+        value: false
+      },
+      /** Determine if the check is disabled or not */
+      disabled: {
+        type: Boolean,
+        value: false
+      }
     }
   }
 
-  /*----------
-  EVENT HANDLERS
-  ----------*/
-  _updateActive(evt) {
-    let t = evt.target;
-    let index = Number(t.getAttribute('data-index'));
-    let active = this.active;
 
-    if(t.checked) {
-      active.push(index);
-    } else {
-      let i = active.indexOf(index);
-      active.splice(i, 1);
+  /***
+   *     _     _  __
+   *    | |   (_)/ _|
+   *    | |    _| |_ ___
+   *    | |   | |  _/ _ \
+   *    | |___| | ||  __/
+   *    \_____/_|_| \___|
+   *
+   *
+   */
+
+  ready() {
+    if (typeof label === 'undefined') {
+      console.warn('WARNING <checkbox-clab>: "label" not specified');
+      return false;
     }
-
-    this.set('active', active);
   }
 
 
+  /***
+   *    ___  ___     _   _               _
+   *    |  \/  |    | | | |             | |
+   *    | .  . | ___| |_| |__   ___   __| |___
+   *    | |\/| |/ _ \ __| '_ \ / _ \ / _` / __|
+   *    | |  | |  __/ |_| | | | (_) | (_| \__ \
+   *    \_|  |_/\___|\__|_| |_|\___/ \__,_|___/
+   *
+   *
+   */
 
-  /*----------
-  COMPUTE
-  ----------*/
+  /**
+   * Toggle checked or not of the checkbox
+   */
+  toggleActive() {
+    this.set('active', !this.active)
+    return new Promise(resolve => resolve(true))
+  }
+
+  /***
+   *    ______     _            _
+   *    | ___ \   (_)          | |
+   *    | |_/ / __ ___   ____ _| |_ ___  ___
+   *    |  __/ '__| \ \ / / _` | __/ _ \/ __|
+   *    | |  | |  | |\ V / (_| | ||  __/\__ \
+   *    \_|  |_|  |_| \_/ \__,_|\__\___||___/
+   *
+   *
+   */
+
+
+  /***
+   *     _     _     _
+   *    | |   (_)   | |
+   *    | |    _ ___| |_ ___ _ __   ___ _ __ ___
+   *    | |   | / __| __/ _ \ '_ \ / _ \ '__/ __|
+   *    | |___| \__ \ ||  __/ | | |  __/ |  \__ \
+   *    \_____/_|___/\__\___|_| |_|\___|_|  |___/
+   *
+   *
+   */
+
+  _onChange(evt) {
+    this.toggleActive().then(res => {
+      this.dispatchEvent(new CustomEvent('selected-change'))
+    })
+  }
+
+  /***
+   *     _____                             _
+   *    /  __ \                           | |
+   *    | /  \/ ___  _ __ ___  _ __  _   _| |_ ___ _ __ ___
+   *    | |    / _ \| '_ ` _ \| '_ \| | | | __/ _ \ '__/ __|
+   *    | \__/\ (_) | | | | | | |_) | |_| | ||  __/ |  \__ \
+   *     \____/\___/|_| |_| |_| .__/ \__,_|\__\___|_|  |___/
+   *                          | |
+   *                          |_|
+   */
+
+  /**
+   * Compute style of the wrapper
+   */
   _computeType(type) {
     return ['row', type].join(' ');
-  }
-
-
-
-  /*----------
-  UTILS
-  ----------*/
-  _checkIfTrue(i, n) {
-    switch(typeof n) {
-      case 'number':
-        return i == n;
-        break;
-      case 'object':
-        for(let x = 0; x < n.length; x++) {
-          if(i == n[x]) return true;
-        }
-        break;
-    }
   }
 
 }
