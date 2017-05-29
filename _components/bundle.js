5800878a1ee8dfa610bc71aa7eb4fa9e8d03088f
Salvatore Laisa <moebiusmania@gmail.com>
2017-05-29 17:35:36 +0200

    v1.3.2 (#146)
    * Modal: enabled possibility to have the modal open right after init (#145)
    
    * build for v1.3.2

--- _components/bundle.js
+++ _components/bundle.js
@@ -20077,7 +20077,7 @@ var FileClab = exports.FileClab = function () {
 
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+  value: true
 });
 exports.ModalClab = undefined;
 
@@ -20092,217 +20092,215 @@ var _script = __webpack_require__(4);
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var ModalClab = exports.ModalClab = function () {
-	function ModalClab() {
-		_classCallCheck(this, ModalClab);
-	}
+  function ModalClab() {
+    _classCallCheck(this, ModalClab);
+  }
 
-	_createClass(ModalClab, [{
-		key: "beforeRegister",
-		value: function beforeRegister() {
-			this.is = 'modal-clab';
-			this.properties = {
-				title: {
-					type: String,
-					value: 'Modal title'
-				},
-				visible: {
-					type: Boolean,
-					value: false,
-					observer: '_animateShowHide'
-				},
-				primary: {
-					type: String,
-					value: null
-				},
-				secondary: {
-					type: String,
-					value: null
-				},
-				warning: {
-					type: String,
-					value: null
-				},
-				primaryDisabled: {
-					type: Boolean,
-					value: false
-				},
-				content: {
-					type: String,
-					value: null
-				},
-				stopClose: {
-					type: Boolean,
-					value: false
-				},
-				width: {
-					type: Number,
-					value: 840
-				},
-				noAnimation: {
-					type: Boolean,
-					value: false
-				},
-				noActions: {
-					type: Boolean,
-					value: false
-				}
-			};
-		}
-	}, {
-		key: "attached",
-		value: function attached() {
-			var _this = this;
+  _createClass(ModalClab, [{
+    key: "beforeRegister",
+    value: function beforeRegister() {
+      this.is = 'modal-clab';
+      this.properties = {
+        title: {
+          type: String,
+          value: 'Modal title'
+        },
+        visible: {
+          type: Boolean,
+          value: false,
+          observer: '_animateShowHide'
+        },
+        primary: {
+          type: String,
+          value: null
+        },
+        secondary: {
+          type: String,
+          value: null
+        },
+        warning: {
+          type: String,
+          value: null
+        },
+        primaryDisabled: {
+          type: Boolean,
+          value: false
+        },
+        content: {
+          type: String,
+          value: null
+        },
+        stopClose: {
+          type: Boolean,
+          value: false
+        },
+        width: {
+          type: Number,
+          value: 840
+        },
+        noAnimation: {
+          type: Boolean,
+          value: false
+        },
+        noActions: {
+          type: Boolean,
+          value: false
+        }
+      };
+    }
+  }, {
+    key: "attached",
+    value: function attached() {
+      var _this = this;
 
-			// Preparing the animations
-			if (!this.noAnimation) {
-				var opacity = [{
-					opacity: 0
-				}, {
-					opacity: 1
-				}];
-				var scale = [{
-					transform: 'scale(.95)'
-				}, {
-					transform: 'scale(1)'
-				}];
+      // Preparing the animations
+      if (!this.noAnimation) {
+        var opacity = [{
+          opacity: 0
+        }, {
+          opacity: 1
+        }];
+        var scale = [{
+          transform: 'scale(.95)'
+        }, {
+          transform: 'scale(1)'
+        }];
 
-				this.modalEnter = function (target) {
-					return new GroupEffect([new KeyframeEffect(target, opacity, {
-						duration: 190,
-						fill: 'forwards',
-						direction: 'normal'
-					}), new KeyframeEffect(_this.querySelector('.modal'), scale, {
-						duration: 190,
-						fill: 'forwards',
-						direction: 'normal'
-					})]);
-				};
-				this.modalExit = function (target) {
-					return new GroupEffect([new KeyframeEffect(target, opacity, {
-						duration: 150,
-						fill: 'forwards',
-						direction: 'reverse'
-					}), new KeyframeEffect(_this.querySelector('.modal'), scale, {
-						duration: 150,
-						fill: 'forwards',
-						direction: 'reverse'
-					})]);
-				};
-			}
-		}
-	}, {
-		key: "detached",
-		value: function detached() {
-			document.querySelector('body').classList.remove('no-scroll');
-		}
-	}, {
-		key: "_computeWidth",
-		value: function _computeWidth(width) {
-			var str = 'max-width:' + width + 'px';
-			return str;
-		}
+        this.modalEnter = function (target) {
+          return new GroupEffect([new KeyframeEffect(target, opacity, {
+            duration: 190,
+            fill: 'forwards',
+            direction: 'normal'
+          }), new KeyframeEffect(_this.querySelector('.modal'), scale, {
+            duration: 190,
+            fill: 'forwards',
+            direction: 'normal'
+          })]);
+        };
+        this.modalExit = function (target) {
+          return new GroupEffect([new KeyframeEffect(target, opacity, {
+            duration: 150,
+            fill: 'forwards',
+            direction: 'reverse'
+          }), new KeyframeEffect(_this.querySelector('.modal'), scale, {
+            duration: 150,
+            fill: 'forwards',
+            direction: 'reverse'
+          })]);
+        };
+      }
+    }
+  }, {
+    key: "detached",
+    value: function detached() {
+      document.querySelector('body').classList.remove('no-scroll');
+    }
+  }, {
+    key: "_computeWidth",
+    value: function _computeWidth(width) {
+      var str = 'max-width:' + width + 'px';
+      return str;
+    }
 
-		/*----------
-  	EVENT HANDLERS
-  ----------*/
+    /*----------
+    	EVENT HANDLERS
+    ----------*/
 
-	}, {
-		key: "_closeModal",
-		value: function _closeModal(evt) {
-			evt.stopPropagation();
-			if (!this.stopClose) this.visible = false;
-			this.dispatchEvent(new CustomEvent('close', {
-				bubbles: true,
-				composed: true
-			}));
-		}
-	}, {
-		key: "_block",
-		value: function _block(evt) {
-			evt.stopPropagation();
-		}
-	}, {
-		key: "_primaryAction",
-		value: function _primaryAction(evt) {
-			this.dispatchEvent(new CustomEvent('modal-primary', {
-				bubbles: true,
-				composed: true
-			}));
-		}
-	}, {
-		key: "_secondaryAction",
-		value: function _secondaryAction(evt) {
-			this.dispatchEvent(new CustomEvent('modal-secondary', {
-				bubbles: true,
-				composed: true
-			}));
-		}
-	}, {
-		key: "_warningAction",
-		value: function _warningAction(evt) {
-			this.dispatchEvent(new CustomEvent('modal-warning', {
-				bubbles: true,
-				composed: true
-			}));
-		}
+  }, {
+    key: "_closeModal",
+    value: function _closeModal(evt) {
+      evt.stopPropagation();
+      if (!this.stopClose) this.visible = false;
+      this.dispatchEvent(new CustomEvent('close', {
+        bubbles: true,
+        composed: true
+      }));
+    }
+  }, {
+    key: "_block",
+    value: function _block(evt) {
+      evt.stopPropagation();
+    }
+  }, {
+    key: "_primaryAction",
+    value: function _primaryAction(evt) {
+      this.dispatchEvent(new CustomEvent('modal-primary', {
+        bubbles: true,
+        composed: true
+      }));
+    }
+  }, {
+    key: "_secondaryAction",
+    value: function _secondaryAction(evt) {
+      this.dispatchEvent(new CustomEvent('modal-secondary', {
+        bubbles: true,
+        composed: true
+      }));
+    }
+  }, {
+    key: "_warningAction",
+    value: function _warningAction(evt) {
+      this.dispatchEvent(new CustomEvent('modal-warning', {
+        bubbles: true,
+        composed: true
+      }));
+    }
 
-		/*----------
-  	OBSERVERS
-  ----------*/
+    /*----------
+    	OBSERVERS
+    ----------*/
 
-	}, {
-		key: "_animateShowHide",
-		value: function _animateShowHide(val, oldval) {
-			var target = this.querySelector('.modal-overlay');
+  }, {
+    key: "_animateShowHide",
+    value: function _animateShowHide(val, oldval) {
+      var target = this.querySelector('.modal-overlay');
 
-			if (oldval != undefined) {
-				if (val) {
-					document.querySelector('body').classList.add('no-scroll');
-					target.style.display = 'block';
-					if (!this.noAnimation) {
-						var animation = this.modalEnter(target);
-						var player = document.timeline.play(animation);
-					} else {
-						target.style.opacity = 1;
-					}
-				} else {
-					document.querySelector('body').classList.remove('no-scroll');
-					if (!this.noAnimation) {
-						var _animation = this.modalExit(target);
-						var _player = document.timeline.play(_animation);
-						this._onAnimationComplete(_player, function () {
-							target.style.display = 'none';
-						});
-					} else {
-						target.style.display = 'none';
-						target.style.opacity = 0;
-					}
-				}
-			}
-		}
+      if (val) {
+        document.querySelector('body').classList.add('no-scroll');
+        target.style.display = 'block';
+        if (!this.noAnimation && this.modalEnter) {
+          var animation = this.modalEnter(target);
+          var player = document.timeline.play(animation);
+        } else {
+          target.style.opacity = 1;
+        }
+      } else {
+        document.querySelector('body').classList.remove('no-scroll');
+        if (!this.noAnimation && this.modalExit) {
+          var _animation = this.modalExit(target);
+          var _player = document.timeline.play(_animation);
+          this._onAnimationComplete(_player, function () {
+            target.style.display = 'none';
+          });
+        } else {
+          target.style.display = 'none';
+          target.style.opacity = 0;
+        }
+      }
+    }
 
-		/*----------
-  	PUBLIC
-  ----------*/
+    /*----------
+    	PUBLIC
+    ----------*/
 
-	}, {
-		key: "show",
-		value: function show() {
-			this.visible = true;
-		}
-	}, {
-		key: "hide",
-		value: function hide() {
-			this.visible = false;
-		}
-	}, {
-		key: "behaviors",
-		get: function get() {
-			return [_behaviors.AnimationsBehavior];
-		}
-	}]);
+  }, {
+    key: "show",
+    value: function show() {
+      this.visible = true;
+    }
+  }, {
+    key: "hide",
+    value: function hide() {
+      this.visible = false;
+    }
+  }, {
+    key: "behaviors",
+    get: function get() {
+      return [_behaviors.AnimationsBehavior];
+    }
+  }]);
 
-	return ModalClab;
+  return ModalClab;
 }();
 
 (0, _polymer.Polymer)(ModalClab);
