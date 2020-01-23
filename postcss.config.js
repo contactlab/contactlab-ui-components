08c8868e7fb9df2da63e19720dc2ba57916a4879
Giovanni Tramutola <14157578+giotramu@users.noreply.github.com>
2020-01-23 09:20:50 +0100

    Optimize the components responsiveness and fix some accessibility issues (#226)
    * make tables and modals responsive
    
    * fix table css classes
    
    * update build
    
    * fix modal info grid
    
    * fix table footers
    
    * fix table footer responsiveness
    
    * fix table-footer grid
    
    * fix modal info grid
    
    * fix height of modal info row
    
    * fix inputs accessibility issues
    
    * fix table responsive
    
    * fix modal-info responsiveness
    
    * decrease number of columns in modal-info
    
    * bump pkg version

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,12 @@
 /*eslint-env node */
 const path = require('path');
+const MQ_SRC =
+  './node_modules/@contactlab/ds-tokens/lib/styles/css/media-queries.css';
 
 module.exports = require('@giotramu/postcss-config').expandDefault({
   map: true,
   plugins: {
-    'postcss-import': false,
+    'postcss-custom-media': {importFrom: MQ_SRC},
     '@csstools/postcss-sass': {
       includePaths: [path.join(__dirname, 'node_modules')]
     }
